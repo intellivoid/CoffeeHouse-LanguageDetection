7348799450b4b36f1afa07f68056a4e8635918fc
netkas <netkas@intellivoid.info>
2020-07-10 02:23:09 -0400

    Minor correction

--- tests/cld.py
+++ tests/cld.py
@@ -3,8 +3,8 @@ import coffeehouse_languagedetection.cld
 en_text = "Hello World! This is an example of a large text paragraph. There is nothing interesting about this " \
           "content. It will be used used to accurately predict the language input. "
 
-es_text = "Hola Mundo! Este es un ejemplo de un párrafo de texto grande. No hay nada interesante sobre este " \
-          "contenido. Se utilizará para predecir con precisión la entrada del idioma. "
+es_text = "Hola Mundo! Este es un ejemplo de un parrafo de texto grande. No hay nada interesante sobre este " \
+          "contenido. Se utilizara para predecir con precision la entrada del idioma. "
 
 print(en_text)
 print(coffeehouse_languagedetection.cld.identify_language(en_text))
